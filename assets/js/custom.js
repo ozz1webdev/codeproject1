6c669814fca91f25ff6dcc30ecb38ca2f9ec6994
ozz1webdev <ozz1webdev@gmail.com>
2024-03-05 23:26:33 +0100

    adding javascript function aboutAnim

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,25 @@
 let container = document.getElementById('container');
+let aboutContainer = document.getElementById('aboutContainer');
 
 function homeAnim() {
     let container = document.getElementById('container');
+    let aboutContainer = document.getElementById('aboutContainer');
 
+    container.classList.remove('containerClear');
     container.classList.remove('containerFinal');
     container.classList.remove('containerStart');
-    container.classList.add('containerFinal'); 
 
+    container.classList.add('containerStart');
+}
+function aboutAnim(){
+    let container = document.getElementById('container');
+    let aboutContainer = document.getElementById('aboutContainer');
+
+    container.classList.remove('containerClear');
+    container.classList.remove('containerFinal');
+    container.classList.remove('containerStart');
+
+    container.classList.add('containerClear');
+    aboutContainer.classList.add('');
+    
 }
