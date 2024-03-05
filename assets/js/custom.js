c3a74b665302a889d955623f677d2ab25aac9e29
ozz1webdev <ozz1webdev@gmail.com>
2024-03-05 22:11:16 +0100

    adding css animation for main container

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,37 +1,10 @@
-let devCard = document.getElementById('id_card_dev');
-let graphCard = document.getElementById('id_card_graph');
-let serverCard = document.getElementById('id_card_server');
+let container = document.getElementById('container');
 
-function card_server_click() {
-    devCard.classList.remove('card_dev_anim2');
-    graphCard.classList.remove('card_graph_anim2');
-    devCard.classList.add('card_dev_anim1');
-    graphCard.classList.add('card_graph_anim1');
-    serverCard.classList.add('card_server_anim2');
+function homeAnim() {
+    let container = document.getElementById('container');
 
-}
-function card_dev_click() {
-    graphCard.classList.remove('card_graph_anim2');
-    serverCard.classList.remove('card_server_anim2');
-    serverCard.classList.add('card_server_anim1');
-    graphCard.classList.add('card_graph_anim1');
-    devCard.classList.add('card_dev_anim2');
+    container.classList.remove('containerFinal');
+    container.classList.remove('containerStart');
+    container.classList.add('containerFinal');
 
-}
-
-function card_graph_click() {
-    devCard.classList.remove('card_dev_anim2');
-    serverCard.classList.remove('card_server_anim2');
-    devCard.classList.add('card_dev_anim1');
-    serverCard.classList.add('card_server_anim1');
-    graphCard.classList.add('card_graph_anim2');
-}
-function home_link () {
-    devCard.classList.remove('card_dev_anim1');
-    serverCard.classList.remove('card_server_anim1');
-    graphCard.classList.remove('card_graph_anim1');
-
-    devCard.classList.remove('card_dev_anim2');
-    serverCard.classList.remove('card_server_anim2');
-    graphCard.classList.remove('card_graph_anim2');
 }
