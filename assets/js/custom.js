71bb967f32ea5bd9f95c2e1240bc7cf863f21fe8
ozz1webdev <ozz1webdev@gmail.com>
2024-03-08 05:14:29 +0100

    rewrite the website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,21 @@
 let container = document.getElementById('container');
 let aboutContainer = document.getElementById('aboutContainer');
 
-function homeAnim() {
+function homeBtn() {
     let container = document.getElementById('container');
     let aboutContainer = document.getElementById('aboutContainer');
 
     container.classList.remove('containerClear');
-    container.classList.remove('containerFinal');
+    container.classList.remove('containerVisible');
     container.classList.remove('containerStart');
     aboutContainer.classList.remove('aboutContainerClear');
     aboutContainer.classList.remove('aboutContainerFinal');
     aboutContainer.classList.remove('aboutContainerStart');
 
     aboutContainer.classList.add('aboutContainerClear');
-    container.classList.add('containerStart');
+    container.classList.add('containerVisible');
 }
-function aboutAnim(){
+function aboutBtn(){
     let container = document.getElementById('container');
     let aboutContainer = document.getElementById('aboutContainer');
 
@@ -32,31 +32,6 @@ function aboutAnim(){
     aboutContainer.classList.add('aboutContainerStart');
     
 }
-
-
-
-//const card1 = document.querySelector('card1');
-const card1 = document.getElementById('card1');
-
-document.addEventListener('mousemove', (e) =>{
-    const card1 = document.getElementById('card1');
-
-    rotateElement(e,card1);
-});
-
-function rotateElement(event,element) {
-    const x = event.clientX; //track X position
-    const y = event.clientY; //track Y position
+function contactBtn () {
     
-    //get middle of the windows
-    const middleX = window.innerWidth / 2;
-    const middleY = window.innerHeight / 2;
-
-    //get offset from middle the distance where mouse is from middle
-    const offsetX = ((x-middleX) / middleX) * 45;
-    const offsetY = ((y-middleY) / middleY) * 45;
-
-    element.style.setProperty("width",500);
-    element.style.setProperty("--rotateX",-1 * offsetY + "deg");
-    element.style.setProperty("--rotateY", offsetX + "deg");
 }
