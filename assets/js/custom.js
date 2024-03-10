06bfb84cfe53173db1bd6de27253e83876abe366
ozz1webdev <ozz1webdev@gmail.com>
2024-03-10 09:30:11 +0100

    fix responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,6 +22,7 @@ function aboutBtn(){
     aboutpage.style.height = "80vh";
 }
 function contactBtn () {
+
     var homepage = document.getElementById('homepage');
     var aboutpage = document.getElementById('aboutpage');
     var contactpage=document.getElementById('contactpage');
@@ -29,14 +30,19 @@ function contactBtn () {
     homepage.style.height = "0vh";
     aboutpage.style.height = "0vh";
     contactpage.style.height = "80vh";
-    
+    submitBtn();
 }
 function submitBtn () {
     var contactName = document.getElementById("contactName");
     var contactMail = document.getElementById("contactMail");
     var contactMsg = document.getElementById("contactMsg");
-    if (contactName.textContent !== "") { // && contactMail.textContent != "" && contactMsg.textContent != "") {
-        var win = document.getElementById("contactMsg").style.height = "300px";
+    var win = document.getElementById("contactMsg");
+    alert
+    if (contactName.textContent == "Write your name here..") { // && contactMail.textContent != "" && contactMsg.textContent != "") {
+        win.style.height = "0px";
+    }
+    else {
+        win.style.height = "300px";
     }
 }
 function msgCloseBtn() {
